17ef43ea16bcbd90601a1b08c6a2f1aeb60fd1c4
juleskreutzer <j.kreutzer@student.fontys.nl>
2015-10-06 10:46:46 +0200

    Opdracht week 5

--- KochFractal/KochFractal/src/calculate/KochManager.java
+++ KochFractal/KochFractal/src/calculate/KochManager.java
@@ -6,8 +6,10 @@
 package calculate;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
+import javafx.application.Platform;
 import jsf31kochfractalfx.JSF31KochFractalFX;
 import timeutil.TimeStamp;
 
@@ -17,52 +19,160 @@ import timeutil.TimeStamp;
  */
 public class KochManager implements Observer {
 
-    private KochFractal kochfractal;
     private JSF31KochFractalFX mContext;
-    private ArrayList<Edge> edgeList;
+    private List<Edge> edgeList;
+    
+    private KochFractal koch;
+    private KochFractal koch1;
+    private KochFractal koch2;
+    private KochFractal koch3;
+    private int count;
+    private List<Thread> threads;
     
     public KochManager(JSF31KochFractalFX mContext)
     {
         this.mContext = mContext;
-        kochfractal = new KochFractal();
-        kochfractal.addObserver(this);
-        edgeList = new ArrayList<Edge>();
+        edgeList = new ArrayList<>();
+        threads = new ArrayList<>();
+        
+        this.koch = new KochFractal();
+        this.koch.addObserver(this);
+        this.koch1 = new KochFractal();
+        this.koch1.addObserver(this);
+        this.koch2 = new KochFractal();
+        this.koch2.addObserver(this);
+        this.koch3 = new KochFractal();
+        this.koch3.addObserver(this);
+        this.count = 0;
+        
+        
     }
     
     public void ChangeLevel(int nextLevel)
     {
-        TimeStamp timer = new TimeStamp();
-        kochfractal.setLevel(nextLevel);
-        edgeList.clear();
-        timer.setBegin();
-        kochfractal.generateBottomEdge();
-        kochfractal.generateLeftEdge();
-        kochfractal.generateRightEdge();
-        timer.setEnd();
-        mContext.setTextCalc(timer.toString());
-        drawEdges();
+        clearAllThreads();
+        resetCount();
+        koch.setLevel(nextLevel);
+        koch1.setLevel(nextLevel);
+        koch2.setLevel(nextLevel);
+        koch3.setLevel(nextLevel);
+        
+        this.edgeList.clear();
+ 
+        
+        Thread end = new Thread(new Runnable() {
+            @Override public void run() {
+                synchronized(getCurrentInstance())
+                {
+                    try{
+                        final TimeStamp ts = new TimeStamp();
+                        ts.setBegin();
+                        getCurrentInstance().wait();
+                        ts.setEnd();
+                        
+                        Platform.runLater(new Runnable() {
+                            @Override public void run() {
+                                mContext.setTextCalc(ts.toString());
+                            }
+                        });
+                        mContext.requestDrawEdges();
+                    }
+                    catch(Exception ex)
+                    {
+                        System.out.print(ex.toString());
+                    }
+                }
+            }
+        });
+        
+        threads.add(end);
+        
+        Thread t1 = new Thread(new Runner(koch1, this, GeneratePart.BOTTOM));
+        threads.add(t1);
+        Thread t2 = new Thread(new Runner(koch2, this, GeneratePart.LEFT));
+        threads.add(t2);
+        Thread t3 = new Thread(new Runner(koch3, this, GeneratePart.RIGHT));
+        threads.add(t3);
+        
+        end.start();
+        t1.start();
+        t2.start();
+        t3.start();
     }
     
     public void drawEdges()
     {
-        TimeStamp timer = new TimeStamp();
-        timer.setBegin();
-        mContext.clearKochPanel();
-        for(Edge edge : edgeList)
+        if(count >= 3)
         {
-            mContext.drawEdge(edge);
+            TimeStamp timer = new TimeStamp();
+            timer.setBegin();
+            mContext.clearKochPanel();
+            for(Edge edge : edgeList)
+            {
+                mContext.drawEdge(edge);
+            }
+            timer.setEnd();
+            mContext.setTextDraw(timer.toString());
+            mContext.setTextNrEdges(String.format("%d", this.koch.getNrOfEdges()));
         }
-        timer.setEnd();
-        mContext.setTextDraw(timer.toString());
-        mContext.setTextNrEdges(String.valueOf(kochfractal.getNrOfEdges()));
     }
 
     @Override
     public void update(Observable o, Object arg) {
-    Edge edge = (Edge) arg;
-    edgeList.add(edge);
+        Edge edge = (Edge) arg;
+        edgeList.add(edge);
+        System.out.print("Start: (" + edge.X1 + "," + edge.Y1 + "), End: (" + edge.X2 + "," + edge.Y2 + ")");
+
     }
     
+    /**
+     * Let all threads disappear - DIE!
+     */
+    public synchronized void clearAllThreads()
+    {
+        for(Thread t : threads)
+        {
+            t.interrupt();
+        }
+        
+        threads.clear();
+    }
     
+    /**
+     * Set count back to 0
+     */
+    public synchronized void resetCount()
+    {
+        this.count = 0;
+    }
+    
+    /**
+     * Increase count by 1
+     * @return current count
+     */
+    public synchronized int addCount()
+    {
+        this.count++;
+        return this.count;
+    }
+    
+    /**
+     * Get the current instance
+     * @return current KochManager instance
+     */
+    public synchronized KochManager getCurrentInstance()
+    {
+        return this;
+    }
+    
+    /**
+     * To make things penuts
+     */
+    public static enum GeneratePart
+    {
+        BOTTOM,
+        LEFT,
+        RIGHT;
+    }
     
 }
---
+++ KochFractal/KochFractal/src/calculate/Runner.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculate;
+
+import calculate.KochManager.GeneratePart;
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ *
+ * @author juleskreutzer
+ */
+public class Runner implements Runnable {
+    
+    final KochFractal koch;
+    final KochManager manager;
+    final GeneratePart part;
+    
+    public Runner(KochFractal koch, KochManager manager, GeneratePart part)
+    {
+        this.koch = koch;
+        this.manager = manager;
+        this.part = part;
+    }
+    
+    @Override
+    public void run() {
+        try {
+            if(Thread.interrupted())
+                throw new InterruptedException("Oops.. Thread interrupted");
+            
+            switch(part)
+            {
+                case BOTTOM:
+                    koch.generateBottomEdge();
+                    break;
+                case RIGHT:
+                    koch.generateRightEdge();
+                    break;
+                case LEFT:
+                    koch.generateLeftEdge();
+                    break;
+                default:
+                    throw new Exception("Hmm.. Switch screwed up!");
+            }
+            synchronized(manager) {
+            if(manager.addCount() >= 3)
+                manager.notify();
+            } 
+        }
+        catch(Exception e)
+        {
+            System.out.print(e.toString());
+        }
+    }
+    
+}
